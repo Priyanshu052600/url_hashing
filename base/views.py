e92c40ec17149c29184c3ca7ba35af7362bf8556
Priyanshu052600 <98660510+Priyanshu052600@users.noreply.github.com>
2022-04-08 18:09:43 +0530

    Update views.py

--- base/views.py
+++ base/views.py
@@ -1,4 +1,6 @@
-import imp
+import qrcode
+import qrcode.image.svg
+from io import BytesIO
 from django.shortcuts import render
 from django.http import HttpResponse
 from base.models import Url
@@ -9,9 +11,14 @@ def createurl(request):
         full_url=request.POST.get('full_url')
         Code=request.POST.get('code')
         obj=Url.create(full_url,Code)
+        factory = qrcode.image.svg.SvgImage
+        img = qrcode.make(full_url, image_factory=factory, box_size=20)
+        stream = BytesIO()
+        img.save(stream)
         return render(request,'base/index.html',{
             'full_url' :obj.full_url,
-            'short_url': request.get_host()+'/'+obj.short_url
+            'short_url': request.get_host()+'/'+obj.short_url,
+            'svg':stream.getvalue().decode()
         })
     return render(request,'base/index.html')
 
